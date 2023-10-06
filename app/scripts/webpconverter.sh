af5a7444995094bf92df50b5751fcfdfc7c592d3
Matteo Cancellieri <matteo.cancellieri@open.ac.uk>
2023-10-06 09:30:56 +0100

    Add galleries and no pics

--- app/scripts/webpconverter.sh
+++ app/scripts/webpconverter.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-FILES="$1/*.jpg"
+FILES="$1/*.png"
 
 for f in $FILES
 do
-        echo $f
-	filename="${f##*/.jpg}"
-        extension="${filename##*.}"
-        filename="${filename%.*}"
+  echo $f
+	filename="${f##*/.png}"
+  extension="${filename##*.}"
+  filename="${filename%.*}"
 	echo $filename
 	cwebp -q 80 $f -o ${filename}.webp
 done
